d36f1e1c6824c565d544de8857f66375791d50b3
Nycollas009 <snycollas30@gmail.com>
2026-04-23 19:46:54 -0300

    Correção de bugs no Render

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
+server.listen(port, '0.0.0.0', () => {
+    console.log(`Servidor rodando na porta ${port}`);
+});
+
 app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
