9e2178f143da5cedcdad64185e4b37ce5adf914e
Nycollas009 <snycollas30@gmail.com>
2026-05-31 12:20:12 -0300

    teste de deploy

--- projeto-twitter/server.js
+++ projeto-twitter/server.js
@@ -13,8 +13,7 @@ server.listen(port, '0.0.0.0', () => {
 });
 
 app.use(cors());
-app.use(express.json({ limit: '10mb' }));
-app.use(express.urlencoded({ limit: '10mb', extended: true })); // Permitir parsing de URL-encoded com limite aumentado
+app.use(express.json());
 app.use(express.static('public'));
 
 const DB_FILE = './database.json';
